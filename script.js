ac7d23a0525ef57cedc7e11c60f408207d558c22
dwiprayoga <komangyoga835@gmail.com>
2024-11-13 07:50:53 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdownBtn.addEventListener('click', function() {
+        dropdownContent.classList.toggle('show');
+    });
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropdown-btn')) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    };
+});
